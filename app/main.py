c1eecf1ed3a31260f8fd97485637548874f52d50
mahamudh-official <mymahamud.ha@gmail.com>
2026-09-11 16:39:49 +0600

    first commit

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from sqlmodel import create_engine
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL") 
+
+engine = create_engine(DATABASE_URL)
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Docker FastAPI API"}
+
+@app.get("/db-test")
+def db_test():
+    with engine.connect() as connection:
+        return {"message": "Database connected"}
